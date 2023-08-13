7b0f9d8f290e959bf67dc2ac7328b5b19a5d86e1
Shadi Ghaith <s_gheith@hotmail.com>
2023-08-13 10:56:36 +0300

    lesson-3

--- controllers/semanticSearchController.js
+++ controllers/semanticSearchController.js
@@ -1,5 +1,9 @@
+const fs = require('fs');
+const path = require('path');
 
 const openai = require('../config/openaiConfig')
+
+const CACHE_FILE_PATH = path.join(__dirname, '../embeddings/embeddings.csv');
 
 const generatEembedding = async (text) => {
 
@@ -11,6 +15,70 @@ const generatEembedding = async (text) => {
   //console.log(response.data.data[0].embedding);
 
   return response.data.data[0].embedding
+}
+
+// Function that manages the cache and calls generatEembedding if needed
+const generatEembeddingWithCache = async (text) => {
+  const cacheMap = readCache();
+
+  if (cacheMap.has(text)) {
+    // If the text is in the cache, return the vector from the cache
+    return cacheMap.get(text);
+  } else {
+    // If the text is not in the cache, call generatEembedding and update the cache
+    const embeddingVector = await generatEembedding(text);
+
+    // Update the cache file (append the new entry) only if it doesn't already exist
+    appendToCache(text, embeddingVector);
+
+    return embeddingVector;
+  }
+}
+
+// Function to read the cache from the CSV file and return it as a Map
+function readCache() {
+  try {
+    if (!fs.existsSync(CACHE_FILE_PATH)) {
+      fs.writeFileSync(CACHE_FILE_PATH, 'text\tembedding\n', 'utf8');
+      return new Map();
+    }
+
+    const cacheFile = fs.readFileSync(CACHE_FILE_PATH, 'utf8');
+    const lines = cacheFile.trim().split('\n');
+    const cacheMap = new Map();
+
+    // Start the loop from index 1 to skip the header line
+    for (let i = 1; i < lines.length; i++) {
+      const line = lines[i].trim();
+      if (line) {
+        const [text, embedding] = line.split('\t');
+        cacheMap.set(text, JSON.parse(embedding));
+      }
+    }
+
+    return cacheMap;
+  } catch (error) {
+    return new Map();
+  }
+}
+
+// Function to append the updated cache to the CSV file
+function appendToCache(text, embeddingVector) {
+  const cacheMap = readCache();
+  if (cacheMap.has(text)) {
+    // If the entry already exists, no need to append again
+    return;
+  }
+
+  const vectorString = JSON.stringify(embeddingVector);
+  const newCacheLine = `${text}\t${vectorString}\n`;
+
+  // Append to the file
+  fs.appendFileSync(CACHE_FILE_PATH, newCacheLine, 'utf8');
+
+  // Update the cacheMap with the new entry
+  cacheMap.set(text, embeddingVector);
+}
 }
 
 const cosineSimilarity = (vecA, vecB) => {
@@ -33,4 +101,4 @@ const dotProduct = (vecA, vecB) => {
   return product;
 };
 
-module.exports = { generatEembedding, cosineSimilarity }
+module.exports = { generatEembedding, cosineSimilarity, generatEembeddingWithCache }
--- playground.js
+++ playground.js
@@ -1,13 +1,13 @@
-const { generatEembedding, cosineSimilarity } = require('./controllers/semanticSearchController');
+const { generatEembedding, cosineSimilarity, generatEembeddingWithCache } = require('./controllers/semanticSearchController');
 
 text1 = 'The boy playing in the street';
-text2 = 'The kid having fun in the raod';
-//text2 = 'It is a wonderfull morning';
+// text2 = 'The kid having fun in the raod';
+text2 = 'It is a wonderfull morning';
 
-generatEembedding(text1)
+generatEembeddingWithCache(text1)
   .then( embedding1 => {
     //console.log(embedding)
-    generatEembedding(text2).then( embedding2 => {
+    generatEembeddingWithCache(text2).then( embedding2 => {
       //console.log(embedding2)
       console.log(cosineSimilarity(embedding1, embedding2))  
   })
